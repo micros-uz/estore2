05cdbc09bb33e39fcf82a5ba0f7893addd56de95
Artem <www.ww>
2014-01-16 20:11:44 +0500

    Added: Simple ThymeLeaf FORM GET and POST

--- src/main/java/uz/micros/estore/controller/AdminController.java
+++ src/main/java/uz/micros/estore/controller/AdminController.java
@@ -2,9 +2,13 @@ package uz.micros.estore.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import uz.micros.estore.controller.viewmodels.ExecParams;
 import uz.micros.estore.util.DbManager;
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/admin")
@@ -21,6 +25,22 @@ public class AdminController {
 
     @RequestMapping(value = "exec", method = RequestMethod.GET)
     public String exec(ModelMap model) {
+        ExecParams foo = new ExecParams();
+        foo.setQuery("bar");
+
+        model.addAttribute("ep", foo);
+
+        return "admin/adminExec";
+    }
+
+    @RequestMapping(value = "exec", method = RequestMethod.POST)
+    public String execPost(ModelMap model, @ModelAttribute(value="ep") ExecParams params) {
+        model.addAttribute("header1", "fefe");
+        ArrayList<String> data = new ArrayList<>();
+        data.add(params.getQuery());
+        data.add("fefe");
+        model.addAttribute("data", data);
+
         return "admin/adminExec";
     }
 }
---
+++ src/main/java/uz/micros/estore/controller/viewmodels/ExecParams.java
@@ -0,0 +1,13 @@
+package uz.micros.estore.controller.viewmodels;
+
+public class ExecParams {
+    private String query;
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+}
