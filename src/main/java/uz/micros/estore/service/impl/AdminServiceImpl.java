bbdaf38c5f3a613e897a14c58bb6f8bb21464e58
micros-uz <www.ww>
2014-01-17 00:00:56 +0500

    Added: Admin Service

---
+++ src/main/java/uz/micros/estore/service/impl/AdminServiceImpl.java
@@ -0,0 +1,12 @@
+package uz.micros.estore.service.Impl;
+
+import uz.micros.estore.service.AdminService;
+
+import java.util.List;
+
+public class AdminServiceImpl extends AdminService {
+    @Override
+    public List<String> run(String query) {
+        return null;
+    }
+}
--- src/main/java/uz/micros/estore/util/DbManager.java
+++ src/main/java/uz/micros/estore/util/DbManager.java
@@ -1,46 +1,68 @@
 package uz.micros.estore.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import javafx.util.Pair;
+
+import java.sql.*;
 
 public class DbManager {
-    public static String connectToDb() {
+    public static Pair<String, Connection> connectToDb() {
+        String res1 = null;
+        Connection res2 = null;
+
         System.out.println("-------- PostgreSQL "
                 + "JDBC Connection Testing ------------");
 
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
-            return "Where is your PostgreSQL JDBC Driver? "
+            res1 = "Where is your PostgreSQL JDBC Driver? "
                     + "Include in your library path!";
         }
 
-        System.out.println("PostgreSQL JDBC Driver Registered!");
+        if (res1 == null) {
+            System.out.println("PostgreSQL JDBC Driver Registered!");
 
-        Connection connection = null;
+            try {
+                String host = System.getenv("OPENSHIFT_POSTGRESQL_DB_HOST");
+                String port = System.getenv("OPENSHIFT_POSTGRESQL_DB_PORT");
 
-        try {
-            String host = System.getenv("OPENSHIFT_POSTGRESQL_DB_HOST");
-            String port = System.getenv("OPENSHIFT_POSTGRESQL_DB_PORT");
+                if (host == null) {
+                    res2 = DriverManager.getConnection(
+                            "jdbc:postgresql://127.0.0.1:5432/estore", "postgres",
+                            "dev1234");
+                } else {
+                    res2 = DriverManager.getConnection(
+                            "jdbc:postgresql://" + host + ":" + port + "/estore", "adminn8szx56",
+                            "yqAhZ7uXKt6Y");
+                }
 
-            if (host == null) {
-                connection = DriverManager.getConnection(
-                        "jdbc:postgresql://127.0.0.1:5432/estore", "postgres",
-                        "dev1234");
-            } else {
-                connection = DriverManager.getConnection(
-                        "jdbc:postgresql://" + host + ":" + port + "/estore", "adminn8szx56",
-                        "yqAhZ7uXKt6Y");
+            } catch (SQLException e) {
+                res1 = "Connection Failed! Check output console " + e.getMessage();
             }
 
-        } catch (SQLException e) {
-            return "Connection Failed! Check output console " + e.getMessage();
+            if (res2 != null)
+                res1 = " You made it, take control your database now!";
+            else
+                res1 = " Failed to make connection!";
         }
 
-        if (connection != null)
-            return " You made it, take control your database now!";
-        else
-            return " Failed to make connection!";
+        return new Pair<>(res1, res2);
+    }
+
+    public static String runQuery(String query) {
+        Pair<String, Connection> res = connectToDb();
+
+        if (res.getValue() != null) {
+            try{
+                Statement st = res.getValue().createStatement();
+                ResultSet rs = st.executeQuery(query);
+
+                return rs.toString();
+            }
+            catch(SQLException ex){
+                return ex.getMessage();
+            }
+        } else
+            return res.getKey();
     }
 }
