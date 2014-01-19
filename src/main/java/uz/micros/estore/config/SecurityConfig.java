d065fafe062f71c1a0f418ce528e19c067ec6cf0
micros-uz <www.ww>
2014-01-20 00:05:40 +0500

    Added: SecurityConfig compiled!

--- src/main/java/uz/micros/estore/config/SecurityConfig.java
+++ src/main/java/uz/micros/estore/config/SecurityConfig.java
@@ -2,16 +2,41 @@ package uz.micros.estore.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 //http://docs.spring.io/spring-security/site/docs/3.2.x/guides/helloworld.html
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
+        auth.inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
     }
+
+
+/*    @Order(1)
+    public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void registerAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication().withUser("api").password("pass").roles("API");
+        }
+
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeUrls()
+                    .antMatchers("/api*//**").hasRole("API")
+                    .and()
+                    .httpBasic();
+        }
+    }*/
 }
+
