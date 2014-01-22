f1191e1951c10ad1c6b789d5f965c6d17d483fe6
micros-uz <www.ww>
2014-01-23 00:11:04 +0500

    Added: Login Controller and Login View

---
+++ src/main/java/uz/micros/estore/config/SecurityRegistration.java
@@ -0,0 +1,14 @@
+package uz.micros.estore.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/*public class SecurityRegistration extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
+
+        @Override
+        protected Class<?>[] getRootConfigClasses() {
+            return new Class[] { SecurityConfig.class };
+        }
+
+    }
+}*/
