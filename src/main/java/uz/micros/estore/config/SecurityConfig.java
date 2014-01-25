f511ea5d7aa98e41e2b3d96b6a50017929c32a97
Artem <www.ww>
2014-01-25 17:07:16 +0500

    Added: Login Controller (previous was not used) and Custom Login Page (simple) + JQuery WEBJARS and navbar login dropdown

--- src/main/java/uz/micros/estore/config/SecurityConfig.java
+++ src/main/java/uz/micros/estore/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package uz.micros.estore.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
@@ -18,5 +19,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth
                 .inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and();
+        http.formLogin()
+                .loginPage("/login")
+                .permitAll();
     }
 }
